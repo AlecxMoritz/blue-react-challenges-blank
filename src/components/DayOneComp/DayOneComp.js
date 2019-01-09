4c5af7d1124da5eaffbce40ba9331e288a8fc452
Alecx Moritz <hanelore@live.com>
2019-01-09 09:07:41 -0500

    adding all files

---
+++ src/components/DayOneComp/DayOneComp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DayOneComp = (props) => {
+  return(
+    <h1>This is a day 1 component!</h1>
+  )
+}
+
+export default DayOneComp;
---
+++ src/components/DayTwoComp/DayTwoComp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DayTwoComp = (props) => {
+  return(
+    <h1>This is a day 2 component!</h1>
+  )
+}
+
+export default DayTwoComp;
